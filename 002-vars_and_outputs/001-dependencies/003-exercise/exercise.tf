c4ae552775639a16afa2eaa82c4dd1dfc7a74254
Ariel Agranovich <ariel.agra@gmail.com>
2026-02-08 18:41:46 +0200

    added comments to clarify

--- 002-vars_and_outputs/001-dependencies/003-exercise/exercise.tf
+++ 002-vars_and_outputs/001-dependencies/003-exercise/exercise.tf
@@ -95,7 +95,7 @@ resource "aws_route_table" "public" {
 resource "aws_route" "public_internet_access" {
   route_table_id         = XXX
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = XXX
+  gateway_id             = XXX # (internet gateway (from part 1))
 }
 
 resource "aws_route_table" "private" {
@@ -109,7 +109,7 @@ resource "aws_route_table" "private" {
 resource "aws_route" "private_nat_access" {
   route_table_id         = XXX
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = XXX
+  nat_gateway_id         = XXX # (nat gateway (from part 3))
 }
 
 # ============================================

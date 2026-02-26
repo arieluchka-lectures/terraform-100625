5ebb0827c399e9f532e3de4ad3a02d2c74e8b965
Sergey Gavrielov <sergey.gav82@gmail.com>
2026-02-26 11:41:42 +0200

    commited start_script file

--- 010-One_Click_Apps/Stirling-PDF/Sergey/start_script.sh
+++ 010-One_Click_Apps/Stirling-PDF/Sergey/start_script.sh
@@ -2,8 +2,32 @@
 
 sudo yum update -y 
 
+# Install docker
 sudo yum install docker -y
 
-sudo service docker start
+sudo systemctl start docker
 
-git clone https://github.com/arieluchka-lectures/terraform-100625/tree/Sergey/010-One_Click_Apps/Stirling-PDF/Sergey
+sudo systemctl enable docker
+
+# Install docker compose plugin 
+sudo yum install docker-compose-plugin -y
+
+# Create app directory
+mkdir -p /home/ec2-user/sergey-app
+
+cd /home/ec2-user/sergey-app
+
+#install git
+sudo yum install git -y
+
+git clone https://github.com/arieluchka-lectures/terraform-100625/
+
+cd terraform-100625
+
+# Switch branch
+git switch Sergey
+
+# Navigate to the docker compose file location
+cd 010-One_Click_Apps/Stirling-PDF/Sergey
+
+sudo docker compose up -d

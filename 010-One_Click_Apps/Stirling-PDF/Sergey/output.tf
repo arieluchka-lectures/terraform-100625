1dd483eae41fc3c7c4cb0d38b7f785c53d005526
Sergey Gavrielov <sergey.gav82@gmail.com>
2026-03-03 16:19:16 +0200

    commited output file

--- 010-One_Click_Apps/Stirling-PDF/Sergey/output.tf
+++ 010-One_Click_Apps/Stirling-PDF/Sergey/output.tf
@@ -8,9 +8,6 @@ output "combined_message" {
   public_ip:
   ${aws_instance.app_server.public_ip}
   
-  ssh server connect command:
-  ssh -i ${local_file.private_key.filename} ec2-user@${aws_instance.app_server.public_ip}
-
 
 EOT
 }

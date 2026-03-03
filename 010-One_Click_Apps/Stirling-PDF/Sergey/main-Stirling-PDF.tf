9a6890bc36e57d6d6a0fd9fd9f969e80e22644b6
Sergey Gavrielov <sergey.gav82@gmail.com>
2026-03-03 16:18:41 +0200

    commited main file

--- 010-One_Click_Apps/Stirling-PDF/Sergey/main-Stirling-PDF.tf
+++ 010-One_Click_Apps/Stirling-PDF/Sergey/main-Stirling-PDF.tf
@@ -51,10 +51,6 @@ resource "aws_subnet" "public" {
 
 
 # ============================================
-# Part 3: NAT Gateway Setup
-# ============================================
-
-# ============================================
 # Part 4: Route Tables
 # ============================================
 
@@ -95,30 +91,25 @@ resource "aws_route_table_association" "public" {
 resource "aws_security_group" "ec2_sg" {
   name   = "ec2-public-sg"
   vpc_id = aws_vpc.main.id
+}
 
-  # SSH
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
-  # Stirling-PDF
-  ingress {
-    from_port   = 9080
-    to_port     = 9080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # Stirling-PDF_port
+resource "aws_vpc_security_group_ingress_rule" "allow_port" {
+  security_group_id = aws_security_group.ec2_sg.id
+  from_port   = 9080
+  to_port     = 9080
+  ip_protocol = "tcp"
+  cidr_ipv4 =  "0.0.0.0/0"
+}
+
+
 
   # Outbound 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.ec2_sg.id
+  ip_protocol = "-1"
+  cidr_ipv4  = "0.0.0.0/0"
 }
 
 # ============================================
@@ -128,7 +119,7 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0532be01f26a3de55"
   instance_type = "t3.small"
   subnet_id     = aws_subnet.public.id 
-  key_name = aws_key_pair.public_key.key_name
+  
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]  
 
   tags = {
@@ -139,18 +130,3 @@ resource "aws_instance" "app_server" {
 
 }
 
-resource "tls_private_key" "ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "local_file" "private_key" {
-  content         = tls_private_key.ssh_key.private_key_pem
-  filename        = "C:/Users/User/.ssh/private_key_excercise.pem"
-  file_permission = "0400"
-}
-
-resource "aws_key_pair" "public_key" {
-  key_name = "ssh_key"
-  public_key = tls_private_key.ssh_key.public_key_openssh
-}

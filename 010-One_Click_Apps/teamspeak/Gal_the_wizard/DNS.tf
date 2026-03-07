658950686ef1962a19d0af7d8b4651262d145728
gal <newmangal@msn.com>
2026-03-07 21:25:21 +0200

    fixed A records imported the dns zone  gave it a cname corrected the dns file structure for easier scaling still need to check app connectivity internally might install xfce to further check it

--- 010-One_Click_Apps/teamspeak/Gal_the_wizard/DNS.tf
+++ 010-One_Click_Apps/teamspeak/Gal_the_wizard/DNS.tf
@@ -1,9 +1,12 @@
 # ============================================
 # Part 7: Dns
 # ============================================
+
+#=============================================
+# Zones & associations
+#=============================================
 resource "aws_route53_zone" "public_zone" {
   name = "wizardnet.100625.lol"
-  # zone_id = "Z091686525OM56S61OWF3" #the id for the zone wizardnet.100625.lol
   tags = {
     Name = "public-zone"
     date = var.daily_date_tag
@@ -15,12 +18,26 @@ resource "aws_route53_zone_association" "public_zone_association" {
   vpc_id  = aws_vpc.main.id
 }
 
+
+
+
+#=============================================  
+# Records
+#============================================
 resource "aws_route53_record" "load_balancer_record" {
   zone_id = aws_route53_zone.public_zone.zone_id
   name    = "teamspeak.wizardnet.100625.lol"
   type    = "A"
   ttl     = 300
-  records = [aws_lb.loadbalancer.dns_name]
+  records = [aws_eip.lb.public_ip]
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.public_zone.zone_id
+  name    = "www.wizardnet.100625.lol"
+  type    = "CNAME"
+  ttl     = 300
+  records = ["wizardnet.100625.lol"]
 }
 
 resource "aws_route53_record" "app_server_record" {

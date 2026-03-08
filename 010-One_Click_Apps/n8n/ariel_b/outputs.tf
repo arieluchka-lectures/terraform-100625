fcff40d3c5423199f1fd217cb36e80a7ddf1565b
Ariel Ben Michael <abm2511@gmail.com>
2026-03-08 20:10:50 +0200

    updated script (without sleep cmds), added link to outputs for the service, and added git ignore file

--- 010-One_Click_Apps/n8n/ariel_b/outputs.tf
+++ 010-One_Click_Apps/n8n/ariel_b/outputs.tf
@@ -2,9 +2,15 @@ output "server_public_id" {
   value = aws_eip.n8n-eip.public_ip
 }
 
+output "current_time_function" {
+  value = timestamp()
+}
+
 output "instructions" {
   value = <<-EOT
         Welcome!
-        Connect to the web service through the server_public_id and port 5678 (maybe needs 3-5 minutes to load)
+        Connect to the web service through the following link (maybe needs 3-5 minutes to load)
+
+        http://${aws_eip.n8n-eip.public_ip}:5678/
     EOT
 }
--- 010-One_Click_Apps/n8n/ariel_b/start_script.sh
+++ 010-One_Click_Apps/n8n/ariel_b/start_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sleep 30
-
 sudo yum install docker -y
-
-sleep 10
 
 sudo systemctl start docker
 sudo docker volume create n8n_data
-sudo docker run -d --name n8n -p 5678:5678 -e N8N_HOST=0.0.0.0 -e N8N_PORT=5678 -e N8N_PROTOCOL=http -e N8N_SECURE_COOKIE=false -v n8n_data:/home/node/.n8n docker.n8n.io/n8nio/n8n
+sudo docker run -d --name n8n \
+ -p 5678:5678 -e N8N_HOST=0.0.0.0 \
+ -e N8N_PORT=5678 -e N8N_PROTOCOL=http \
+ -e N8N_SECURE_COOKIE=false \
+ -v n8n_data:/home/node/.n8n docker.n8n.io/n8nio/n8n

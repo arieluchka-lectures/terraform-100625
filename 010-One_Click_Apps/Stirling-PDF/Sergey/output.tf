93e1778de1d486bf6eda2a749c9d4430a8e6d03a
Sergey Gavrielov <sergey.gav82@gmail.com>
2026-03-08 12:58:36 +0200

    commited output file with port variable

--- 010-One_Click_Apps/Stirling-PDF/Sergey/output.tf
+++ 010-One_Click_Apps/Stirling-PDF/Sergey/output.tf
@@ -2,7 +2,7 @@ output "combined_message" {
   value = <<-EOT
 
   To login Stirling PDF page: 
-  http://${aws_instance.app_server.public_ip}:9080/login
+  http://${aws_instance.app_server.public_ip}:${var.app_port}/login
 
 
   public_ip:

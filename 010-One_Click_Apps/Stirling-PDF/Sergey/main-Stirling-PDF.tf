209c88420fb7c98c353b41a9b56a1458a3885b06
Sergey Gavrielov <sergey.gav82@gmail.com>
2026-03-10 11:01:15 +0200

    commited updated main file

--- 010-One_Click_Apps/Stirling-PDF/Sergey/main-Stirling-PDF.tf
+++ 010-One_Click_Apps/Stirling-PDF/Sergey/main-Stirling-PDF.tf
@@ -131,6 +131,15 @@ resource "aws_network_acl" "new_acl" {
     to_port    = 65535
   }
     
+    egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+}
+
     ingress {
         protocol = "tcp"
         rule_no = 100
